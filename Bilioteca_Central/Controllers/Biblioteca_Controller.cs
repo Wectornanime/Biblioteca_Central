56b40c02dd7a9b33da5ab0f97704d53f097c7a39
Vitoria das Dores <vitoria.dores@ufrpe.br>
2024-04-24 17:19:47 -0300

    segunda versão

---
+++ Bilioteca_Central/Controllers/Biblioteca_Controller.cs
@@ -0,0 +1,54 @@
+﻿using Bilioteca_Central.Data_Biblioteca;
+using Bilioteca_Central.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bilioteca_Central.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Biblioteca_Controller : ControllerBase
+    {
+            private readonly Data_Context _context;
+
+        public Biblioteca_Controller(Data_Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Livro>>> GetAllLivros()
+        {
+            var livros = await _context.Livros.ToListAsync();
+
+            return Ok(livros);
+
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<Livro>> GetLivro(int id)
+        {
+            var Livro = await _context.Livros.FindAsync(id);
+            if(Livro == null)
+            {
+                return BadRequest("Hero Not Found");
+            }
+
+            return Ok(Livro);
+        }
+
+        [HttpPost]
+
+        public async Task<ActionResult<List<Livro>>> AddLivro(Livro Livro)
+        {
+            _context.Livros.Add(Livro);
+            await _context.SaveChangesAsync();
+
+            return Ok(await GetAllLivros());
+        }
+
+  
+    }
+}
---
+++ Bilioteca_Central/Data_Biblioteca/Data_Context.cs
@@ -0,0 +1,28 @@
+﻿using Bilioteca_Central.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bilioteca_Central.Data_Biblioteca
+{
+    public class Data_Context : DbContext
+    {
+
+        public Data_Context(DbContextOptions<Data_Context> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Livro> Livros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Emprestimo> Emprestimos { get; set; }
+        public DbSet<Multa> Multas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Livro>().ToTable("Livros");
+            modelBuilder.Entity<Usuario>().ToTable("Usuarios");
+            modelBuilder.Entity<Emprestimo>().ToTable("Emprestimos");
+            modelBuilder.Entity<Multa>().ToTable("Multas");
+        }
+
+    }
+}
